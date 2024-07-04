e8cc885377f4d61e62128efa44aa73daf96dfc99
Alfex4936 <ikr@kakao.com>
2024-07-04 15:25:13 +0900

    Updates

---
+++ precache-manifest.c8d9ba07cfffa32d0669dc08123a869f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7371ea0dbd4e60b6d99b28ca79003c67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50ada7fe1b9c26d2596c",
+    "url": "/static/css/2.d7faf78c.chunk.css"
+  },
+  {
+    "revision": "35d6de40ed0352f7a166",
+    "url": "/static/css/main.5f286688.chunk.css"
+  },
+  {
+    "revision": "50ada7fe1b9c26d2596c",
+    "url": "/static/js/2.8acda575.chunk.js"
+  },
+  {
+    "revision": "35d6de40ed0352f7a166",
+    "url": "/static/js/main.cd00250b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
