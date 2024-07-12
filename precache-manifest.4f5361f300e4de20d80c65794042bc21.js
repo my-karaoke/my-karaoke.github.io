c54cd6e0c79b7e0cf33dc07bd415cfb949c58f78
Alfex4936 <ikr@kakao.com>
2024-07-12 13:53:51 +0900

    Updates

--- precache-manifest.4f5361f300e4de20d80c65794042bc21.js
+++ precache-manifest.4f5361f300e4de20d80c65794042bc21.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a775d2322f026759154ab9ab1653a3d9",
+    "revision": "3fbb312a65e20cfc7ae78844820fce7d",
     "url": "/index.html"
   },
   {
-    "revision": "04b2d3e7ac276a0cd3ce",
+    "revision": "282a126fa18d9f68b20b",
     "url": "/static/css/2.d7faf78c.chunk.css"
   },
   {
-    "revision": "ec9c2f5fe95a0fa3c085",
+    "revision": "1926448ca2b783a60ac2",
     "url": "/static/css/main.6f397299.chunk.css"
   },
   {
-    "revision": "04b2d3e7ac276a0cd3ce",
-    "url": "/static/js/2.1b815528.chunk.js"
+    "revision": "282a126fa18d9f68b20b",
+    "url": "/static/js/2.f55918a7.chunk.js"
   },
   {
-    "revision": "ec9c2f5fe95a0fa3c085",
-    "url": "/static/js/main.260b3dd3.chunk.js"
+    "revision": "1926448ca2b783a60ac2",
+    "url": "/static/js/main.8314ddaf.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
